2f2ffce8b808c6a241ae5c5f3484780353ab12b6
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2022-12-17 22:40:52 +0100

    Add project files.

---
+++ StudentApp/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using StudentApp.Data;
+using StudentApp.Repo;
+using StudentApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<StudentAppContext>(option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("StudentAppContext") ?? throw new InvalidOperationException("Connection string 'StudentAppContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add interfaces (Context)
+builder.Services.AddTransient<IService, Service>();
+builder.Services.AddTransient<IRepo, Repo>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI(s =>
+{
+    s.SwaggerEndpoint("/swagger/v1/swagger.json", "Test API");
+    s.RoutePrefix = "api";
+});
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
